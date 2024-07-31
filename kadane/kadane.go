adc57904504fbba6becc8016d62898b161cbd189
Ltqq <1823994454@qq.com>
2024-07-31 17:07:17 +0800

    variable name

--- kadane/kadane.go
+++ kadane/kadane.go
@@ -9,16 +9,16 @@ package kadane
 // -   [LeetCode 53: Maximum Subarray](https://leetcode.com/problems/maximum-subarray/description/
 // TODO understand why
 func maxSubArray(nums []int) int {
-	max := nums[0]
+	result := nums[0]
 	for i := 1; i < len(nums); i++ {
 		if nums[i]+nums[i-1] > nums[i] {
 			nums[i] += nums[i-1]
 		}
-		if nums[i] > max {
-			max = nums[i]
+		if nums[i] > result {
+			result = nums[i]
 		}
 	}
-	return max
+	return result
 }
 
 //-   [LeetCode 918: Maximum Sum Circular Subarray](https://leetcode.com/problems/maximum-sum-circular-subarray/description/)
