6d4d6db53f2f6c33bf422d471740870e7eb88549
Ltqq <1823994454@qq.com>
2024-08-01 10:54:07 +0800

    solution

--- kadane/kadane.go
+++ kadane/kadane.go
@@ -7,7 +7,6 @@ package kadane
 // #### LeetCode Problems:
 //
 // -   [LeetCode 53: Maximum Subarray](https://leetcode.com/problems/maximum-subarray/description/
-// TODO understand why
 func maxSubArray(nums []int) int {
 	result := nums[0]
 	for i := 1; i < len(nums); i++ {
@@ -16,6 +15,26 @@ func maxSubArray(nums []int) int {
 		}
 		if nums[i] > result {
 			result = nums[i]
+		}
+	}
+	return result
+}
+
+// 1.找到子问题，前i个结尾的最大值 = 前i-1个结尾的最大值 + nums[i]   前i-1个结尾的最大值 > 0
+// or 前i个结尾的最大值 = nums[i] 0
+// 2.初始值 i=0 = nums[0]
+func maxSubArray2(nums []int) int {
+	var result = nums[0]
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] > 0 {
+			dp[i] = dp[i-1] + nums[i]
+		} else {
+			dp[i] = nums[i]
+		}
+		if dp[i] > result {
+			result = dp[i]
 		}
 	}
 	return result
