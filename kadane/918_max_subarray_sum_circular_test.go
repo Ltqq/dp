fe887ab98573a0f720c04a6a58d0f038adba6418
Ltqq <1823994454@qq.com>
2024-08-07 16:57:32 +0800

    maximum sum circular subarray

---
+++ kadane/918_max_subarray_sum_circular_test.go
@@ -0,0 +1,23 @@
+package kadane
+
+import "testing"
+
+func Test_maxSubarraySumCircular(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubarraySumCircular(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubarraySumCircular() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
